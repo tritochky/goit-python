8f05665882aae5f4339a183af1d6123ac001bf02
Olga Fomenko <76599740+tritochky@users.noreply.github.com>
2021-04-15 18:57:19 +0300

    Update hw4.py

--- hw4.py
+++ hw4.py
@@ -1,55 +1,63 @@
-import os
+import pathlib
 import sys
 
-path = sys.argv[1]
-string = 'Start in {}'.format(path)
-print(string)
 
-files = os.listdir(path)
+def sort_files():
+    images = []
+    videos = []
+    documents = []
+    musics = []
+    archives = []
+    unknown = []
+    images_end = []
+    videos_end = []
+    documents_end = []
+    musics_end = []
+    archives_end = []
+    unknown_end = []
+    if path.exists():
+        if path.is_file():
+            for item in path.iterdir():
+                if item.endwith(['jpeg', 'png', 'jpg', 'svg']):
+                    images = images.append(item.name)
+                    images.append('\n')
+                    images_end.append(item.suffix)
+                elif item.endwith(['avi', 'mp4', 'mov', 'mkv']):
+                    videos = videos.append(item.name)
+                    videos.append('\n')
+                    videos_end.append(item.suffix)
+                elif item.endwith(['doc', 'docx', 'txt', 'pdf', 'xlsx', 'pptx']):
+                    documents = documents.append(item.name)
+                    documents.append('\n')
+                    documents_end.append(item.suffix)
+                elif item.endwith(['mp3', 'ogg', 'wav', 'amr']):
+                    musics = musics.append(item.name)
+                    musics.append('\n')
+                    musics_end.append(item.suffix)
+                elif item.endwith(['zip', 'gz', 'tar']):
+                    archives = archives.append(item.name)
+                    archives.append('\n')
+                    archives_end.append(item.suffix)
+                else:
+                    unknown = unknown.append(item.name)
+                    unknown.append('\n')
+                    unknown_end.append(item.suffix)
+        else:
+            sort_files(item)
+        print("Images {} with expensions {}".format(images, images_end))
+        print("Video {} with expensions {}".format(videos, videos_end))
+        print("Documents {} with expensions {}".format(documents, documents_end))
+        print("Music {} with expensions {}".format(musics, musics_end))
+        print("Archives {} with expensions {}".format(archives, archives_end))
+        print("Unknown files {} with expensions {}".format(unknown, unknown_end))
+    else:
+        print("File not exists")
 
-IMAGES = []
-AUDIO = []
-VIDEO = []
-DOCUMENTS = []
-OTHER = []
-EXTENSIONS = set()
 
-REGISTERED_EXTENSIONS = {
-    'JPEG': IMAGES,
-    'PNG': IMAGES,
-    'JPG': IMAGES,
-    'AVI': VIDEO,
-    'MP4': VIDEO,
-    'MOV': VIDEO,
-    'DOC': DOCUMENTS,
-    'DOCX': DOCUMENTS,
-    'TXT': DOCUMENTS,
-    'MP3': AUDIO,
-    'OGG': AUDIO,
-    'WAV': AUDIO,
-    'AMR': AUDIO,
-}
-for file in files:
-    unknown = True
-    for extension in REGISTERED_EXTENSIONS.items():
-        ext, container = extension
-        if file.upper().endswith(ext):
-            EXTENSIONS.add(ext)
-            container.append(file)
-            unknown = False
-            break
-    if unknown:
-        OTHER.append(file)
+def main():
+    path = sys.argv[1]
+    path = pathlib.Path(path)
+    sort_files(path)
 
-img = "Images: {}".format(IMAGES)
-print(img)
-vid = "Video files: {}".format(VIDEO)
-print(vid)
-doc = "Documents: {}".format(DOCUMENTS)
-print(doc)
-aud = "Audio files: {}".format(AUDIO)
-print(aud)
-know = "Unknown files: {}".format(OTHER)
-print(know)
-other = "There are files of types: {}".format(EXTENSIONS)
-print(other)
+if __name__ == '__main__':
+    main()
