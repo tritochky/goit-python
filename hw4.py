174b3ec7782efa46511f022df4bbfd58b7aa1d14
Olga Fomenko <76599740+tritochky@users.noreply.github.com>
2021-05-06 14:36:02 +0300

    Update hw4.py
    This is a working program now!!!!!!

--- hw4.py
+++ hw4.py
@@ -2,54 +2,55 @@ import pathlib
 import sys
 
 
-def sort_files():
-    images = []
-    videos = []
-    documents = []
-    musics = []
-    archives = []
-    unknown = []
-    images_end = []
-    videos_end = []
-    documents_end = []
-    musics_end = []
-    archives_end = []
-    unknown_end = []
+images = []
+videos = []
+documents = []
+musics = []
+archives = []
+unknown = []
+images_end = []
+videos_end = []
+documents_end = []
+musics_end = []
+archives_end = []
+unknown_end = []
+
+def sort_files(path):
+    global images
+    global videos
+    global documents
+    global musics
+    global archives
+    global unknown
+    global images_end
+    global videos_end
+    global documents_end
+    global musics_end
+    global archives_end
+    global unknown_end
     if path.exists():
-        if path.is_file():
+        if path.is_dir():
             for item in path.iterdir():
-                if item.endwith(['jpeg', 'png', 'jpg', 'svg']):
-                    images = images.append(item.name)
-                    images.append('\n')
-                    images_end.append(item.suffix)
-                elif item.endwith(['avi', 'mp4', 'mov', 'mkv']):
-                    videos = videos.append(item.name)
-                    videos.append('\n')
-                    videos_end.append(item.suffix)
-                elif item.endwith(['doc', 'docx', 'txt', 'pdf', 'xlsx', 'pptx']):
-                    documents = documents.append(item.name)
-                    documents.append('\n')
-                    documents_end.append(item.suffix)
-                elif item.endwith(['mp3', 'ogg', 'wav', 'amr']):
-                    musics = musics.append(item.name)
-                    musics.append('\n')
-                    musics_end.append(item.suffix)
-                elif item.endwith(['zip', 'gz', 'tar']):
-                    archives = archives.append(item.name)
-                    archives.append('\n')
-                    archives_end.append(item.suffix)
-                else:
-                    unknown = unknown.append(item.name)
-                    unknown.append('\n')
-                    unknown_end.append(item.suffix)
-        else:
-            sort_files(item)
-        print("Images {} with expensions {}".format(images, images_end))
-        print("Video {} with expensions {}".format(videos, videos_end))
-        print("Documents {} with expensions {}".format(documents, documents_end))
-        print("Music {} with expensions {}".format(musics, musics_end))
-        print("Archives {} with expensions {}".format(archives, archives_end))
-        print("Unknown files {} with expensions {}".format(unknown, unknown_end))
+                sort_files(item)
+        else:    
+            if path.suffix in (['.jpeg', '.png', '.jpg', '.svg']):
+                images.append(path.name)
+                images_end.append(path.suffix)
+            elif path.suffix in (['.avi', '.mp4', '.mov', '.mkv']):
+                videos.append(path.name)
+                videos_end.append(path.suffix)
+            elif path.suffix in (['.doc', '.docx', '.txt', '.pdf', '.xlsx', '.pptx']):
+                documents.append(path.name)
+                documents_end.append(path.suffix)
+            elif path.suffix in (['.mp3', '.ogg', '.wav', '.amr']):
+                musics.append(path.name)
+                musics_end.append(path.suffix)
+            elif path.suffix in (['.zip', '.gz', '.tar']):
+                archives.append(path.name)
+                archives_end.append(path.suffix)
+            else:
+                unknown.append(path.name)
+                unknown_end.append(path.suffix)
     else:
         print("File not exists")
 
@@ -58,6 +59,14 @@ def main():
     path = sys.argv[1]
     path = pathlib.Path(path)
     sort_files(path)
+    print("Images {} with expensions {}".format(images, images_end))
+    print("Video {} with expensions {}".format(videos, videos_end))
+    print("Documents {} with expensions {}".format(documents, documents_end))
+    print("Music {} with expensions {}".format(musics, musics_end))
+    print("Archives {} with expensions {}".format(archives, archives_end))
+    print("Unknown files {} with expensions {}".format(unknown, unknown_end))
+
 
 if __name__ == '__main__':
     main()
+    
